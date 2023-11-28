73f032c0a49204e4850db1c73c10fa57e335ed96
evinsdesign <kelvinsaputra024@gmail.com>
2023-11-28 17:20:07 +0800

    update javascript create item unfinished read

--- script.js
+++ script.js
@@ -53,33 +53,143 @@ document.addEventListener("DOMContentLoaded", function () {
   const dataBooks = [];
   const RENDER_EVENT = "render-data";
   document.addEventListener(RENDER_EVENT, function () {
-    console.log(dataBooks);
+    const unFinishedReading = document.getElementById("unFinishedReading");
+    unFinishedReading.innerHTML = "";
+
+    const finishedReading = document.getElementById("finishedReading");
+    finishedReading.innerHTML = "";
+
+    for (const dataBooksItem of dataBooks) {
+      const booksElement = makeDataBooks(dataBooksItem);
+      if (!dataBooksItem.finishedReading) {
+        unFinishedReading.append(booksElement);
+      } else finishedReading.append(booksElement);
+    }
   });
 
   function makeDataBooks(dataBooksObject) {
-    const title = document.createElement("h3");
+    // field title
+    const title = document.createElement("h4");
     title.innerText = dataBooksObject.textTitle;
-    title.classList.add("pb-4");
+    title.classList.add("py-2", "text-primary", "fw-bold");
 
+    // field author
     const author = document.createElement("p");
-    author.innerText = dataBooksObject.nameAuthor;
+    author.innerText = "Author : " + dataBooksObject.nameAuthor;
 
+    // field year
     const year = document.createElement("p");
-    year.innerText = dataBooksObject.publishYear;
+    year.innerText = "Year : " + dataBooksObject.publishYear;
 
+    // fiel publisher
     const publisher = document.createElement("p");
-    publisher.innerText = dataBooksObject.companyPublisher;
+    publisher.innerText = "Publisher : " + dataBooksObject.companyPublisher;
 
+    // fiel synopsis
     const synopsis = document.createElement("p");
     synopsis.innerText = dataBooksObject.textSynopsis;
+    synopsis.classList.add("text-secondary");
 
+    // container icon button edit, delete
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("d-flex", "gap-2");
+
+    // field textContainer
     const textContainer = document.createElement("div");
     textContainer.classList.add("px-1");
-    textContainer.append(title, author, year, publisher, synopsis);
-
+    textContainer.append(
+      title,
+      author,
+      year,
+      publisher,
+      synopsis,
+      buttonContainer
+    );
     const container = document.createElement("div");
-    container.classList.add("item", "shadow");
+    container.classList.add("my-2");
     container.append(textContainer);
     container.setAttribute("id", `data-${dataBooks.id}`);
+
+    if (dataBooksObject.finishedReading) {
+      const iconRefresh = document.createElement("ion-icon");
+      iconRefresh.setAttribute("name", `refresh`);
+      const refreshButton = document.createElement("button");
+      refreshButton.classList.add(
+        "btn",
+        "btn-danger",
+        "d-flex",
+        "align-items-center",
+        "gap-2"
+      );
+      refreshButton.append(iconRefresh);
+
+      // field trashButton
+      const iconTrash = document.createElement("ion-icon");
+      iconTrash.setAttribute("name", `trash`);
+
+      const trashButton = document.createElement("button");
+      trashButton.innerText = "Hapus Data";
+      trashButton.classList.add(
+        "btn",
+        "btn-danger",
+        "d-flex",
+        "align-items-center",
+        "gap-2"
+      );
+      trashButton.append(iconTrash);
+
+      // field editButton
+      const iconEdit = document.createElement("ion-icon");
+      iconEdit.setAttribute("name", `create-outline`);
+
+      const editButton = document.createElement("button");
+      editButton.innerText = "edit";
+      editButton.classList.add(
+        "btn",
+        "btn-info",
+        "d-flex",
+        "align-items-center",
+        "text-light",
+        "gap-2"
+      );
+      editButton.append(iconEdit);
+      container.append(trashButton, editButton, refreshButton);
+    } else {
+      const iconCheck = document.createElement("ion-icon");
+      iconCheck.setAttribute("name", `checkmark`);
+      const checkButton = document.createElement("button");
+      checkButton.innerText = "Finish";
+      checkButton.classList.add(
+        "btn",
+        "btn-danger",
+        "d-flex",
+        "align-items-center",
+        "gap-2"
+      );
+
+      checkButton.append(iconCheck);
+      checkButton.addEventListener("click", function () {
+        finishedReading(dataBooksObject.id);
+      });
+      container.append(checkButton);
+    }
+    return container;
+  }
+  function finishedReading(booksId) {
+    const booksTarget = findBooks(booksId);
+
+    if (booksTarget == null) return;
+
+    booksTarget.finishedReading = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  function findBooks(booksId) {
+    for (const dataBooksItem of dataBooks) {
+      if (dataBooksItem.id === booksId) {
+        return dataBooksItem;
+      }
+    }
+    return null;
   }
 });
